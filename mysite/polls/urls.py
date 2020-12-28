0c95a4ece148bf1bd5bf11dd5fe21e8e75de8c25
victorstdev <46580792+victorstdev@users.noreply.github.com>
2020-12-28 15:11:04 -0300

    Generic Views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
   # /polls/
-  path('', views.index, name='index'),
+  path('', views.IndexView.as_view(), name='index'),
   # /polls/1/
-  path('<int:question_id>/', views.detail, name='detail'),
+  path('<int:pk>/', views.DetailView.as_view(), name='detail'),
   # /polls/1/results
-  path('<int:question_id>/results/', views.results, name='results'),
+  path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
   # /polls/1/vote
   path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
